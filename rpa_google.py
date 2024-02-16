ce2f7d9b1aec34faf7ed65135ce59588e4640d2f
thiagobluhm <thiagobluhm@gmail.com>
2024-02-16 18:23:09 -0300

    atualizacao do script de rpa do google, contudo segue a necessidade de verificar o porque no COLAB os dados estavam vindo com o SERP em ENGLISH

--- rpa_google.py
+++ rpa_google.py
@@ -7,15 +7,29 @@ num_results = 10
 
 # Alguns sites ocultam o codigo HTML pós DOM permitindo visão apenas do javascript bootstrap, para evitar isso
 # devemos usar o USER-AGENT da página 
+user_agent_ = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+query = 'python'
+num_results = 10
+
+# Alguns sites ocultam o codigo HTML pós DOM permitindo visão apenas do javascript bootstrap, para evitar isso
+# devemos usar o USER-AGENT da página 
 cabecalhos = {
-    "user_agent_" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding":"gzip, deflate, br",
+    "Accept-Language":"en,pt-BR;q=0.9,pt;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+    "Cache-Control":"no-cache",
+    "Pragma":"no-cache",
+    "Upgrade-Insecure-Requests":"1",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0"
 }
 
-search_url = f'https://www.google.com/search?q={query}&num={num_results}'
+search_url = f'https://www.google.com.br/search?q={query}&num={num_results}'
 response = requests.get(search_url, headers=cabecalhos)
 
 soup = BeautifulSoup(response.text, 'lxml')
-# Quando se usa find estamos trazendo um Element.tag
+div_main = soup.find(id="main") 
+divs_ = div_main.find_all('div', class_="N54PNb")
+"""# Quando se usa find estamos trazendo um Element.tag
 div_main = soup.find(id="main") 
 # Quando se usa find_all estamos trazendo um result element, ou uma lista que pode ser "parseada" ou percorrida por indices, no caso
 # um objeto subscritable e aceitará assignment.
@@ -24,4 +38,6 @@ divs_ = div_main.find_all('div')
 print(f"Primeiro índice do find_all = \n {divs_[0]}")
 
 for div in divs_:
-    print(div)
+    print(div)"""
+
+print(divs_[3])
